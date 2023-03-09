8cf62b72030b0325f0398ad34c41157041bba06e
DaniloDeV90 <120051141+DaniloDeV90@users.noreply.github.com>
2023-03-09 19:54:28 -0300

    Add files via upload

---
+++ ExSlides/dowhile.cs
@@ -0,0 +1,53 @@
+﻿
+namespace dowhile
+{
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6];
+            int num = 0;
+            do
+            {
+                int numero;
+                Console.WriteLine("Digite um número: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+                arr[num] = numero;
+
+                num++;
+
+            } while (num != 6);
+            Console.WriteLine("-------");
+
+            var Maior = arr[0];
+            var Menor = arr[0];
+            var result = 0;
+            var tot = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (Menor > arr[i])
+                {
+                    Menor = arr[i];
+                }
+
+                if (Maior < arr[i])
+                {
+                    Maior = arr[i];
+                }
+
+
+                tot += arr[i];
+
+
+                result = tot / arr.Length;
+
+            }
+            Console.WriteLine("O maior número é: " + Maior);
+            Console.WriteLine("O menor número é: " + Menor);
+            Console.WriteLine("A média dos valores é: " + result);
+            Console.ReadLine();
+        }
+    }
+}
+
