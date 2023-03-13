2839fa97241427091d698f8e264ee9ab54ceca41
DaniloDeV90 <120051141+DaniloDeV90@users.noreply.github.com>
2023-03-12 21:29:46 -0300

    Add files via upload

---
+++ ExSlides/vetorImparPar/index.cs
@@ -0,0 +1,66 @@
+﻿using Exercicios_Lista_Um;
+using System.Collections;
+
+namespace VarList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var opcao = "";
+            var result = new ArrayList();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Digite 1 para carregar  um vetor" +
+                    "\nDigite 2 para listar o vetor" +
+                    "\nDigite 3 para mostrar apenas os numeros impares do vetor" +
+                    "\nDigite 4 para mostrar apenas os numeros pares do vetor" +
+                    "\nDigite 5 para mostrar a quantidade de numeros impares no vetor" +
+                    "\nDigite 6 para mostrar a quantidade de numeros pares no vetor" +
+                    "\nDigite 7 para finalizar");
+                opcao = Console.ReadLine();
+
+                Console.Clear();
+                if (opcao == "1")
+                {
+                    CarregarV carregarV = new CarregarV();
+                    result = carregarV.Carregar();
+
+
+
+                }
+                if (opcao =="2")
+                {
+
+                    CarregarV Listar = new CarregarV();
+                    
+                    Console.WriteLine (Listar.Listar(result));
+                }
+                if (opcao == "3")
+                {
+                    CarregarV  impar = new CarregarV();
+                    Console.WriteLine(impar.impar(result));
+                }
+                if (opcao == "4")
+                {
+                    CarregarV par = new CarregarV();
+                    Console.WriteLine(par.par(result));
+                }
+                if (opcao == "5")
+                {
+                    CarregarV qntsimpar = new CarregarV();
+                    Console.WriteLine(qntsimpar.qntsimpar(result));
+                }
+                if (opcao == "6")
+                {
+                    CarregarV qntspar = new CarregarV();
+                    Console.WriteLine(qntspar.qntspar(result));
+                }
+
+            } while (opcao != "7");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExSlides/vetorImparPar/métodos.cs
@@ -0,0 +1,133 @@
+﻿
+using Microsoft.VisualBasic;
+using System.Collections;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Exercicios_Lista_Um
+{
+
+    public class CarregarV
+    {
+
+
+        public ArrayList Carregar()
+        {
+            ArrayList vetor = new ArrayList();
+
+            
+            var stoppU = "";
+            do
+            {
+                Console.WriteLine("Digite o valor do vetor");
+
+                vetor.Add(Convert.ToInt32(Console.ReadLine()));
+
+                Console.WriteLine("Aperte s para parar de adicionar numeros no vetor ou enter para adicionar mais");
+
+               string  stopp = Console.ReadLine();
+                 stoppU = stopp.ToUpper();
+            } while (stoppU != "S" );
+            return vetor;
+
+        }
+
+
+        public string Listar(ArrayList result)
+        {
+
+            var numeros = "";
+            foreach (var item in result)
+            {
+                numeros += item + " ";
+
+            }
+            return "Lista de valores no vetor: " + numeros;
+
+        }
+
+        public string impar(ArrayList ar)
+        {
+            if (ar.Count == 0) return "Nao existem numeros no vetor";
+
+            var impar = "";
+            int numerop = 34;
+
+
+            foreach (int numero in ar)
+            {
+                if (numero % 2 == 1)
+                {
+                    impar += numero + " ";
+
+                }
+
+            }
+
+            if (impar.Length == 0) return "Não existem numeros impares no vetor";
+            return "Os numeros impares do vetor são: " + impar;
+        }
+        public string par(ArrayList ar)
+        {
+
+            if (ar.Count == 0) return "Não existem numeros no vetor";
+
+
+            var par = "";
+
+            foreach (int numero in ar)
+            {
+                if (numero % 2 == 0)
+                {
+                    par += numero + " ";
+
+
+                }
+
+            }
+            if (par.Length == 0) return "Não existem numeros pares no vetor";
+            return "O numeros pares são: " + par;
+        }
+
+
+
+        public string qntsimpar(ArrayList ar)
+        {
+            var impar = 0;
+
+            foreach (int numero in ar)
+            {
+                if (numero % 2 == 1)
+                {
+                    impar++;
+
+                }
+
+            }
+            return "A quantidade de numeros impares são: " + impar;
+
+        }
+
+        public string qntspar(ArrayList ar)
+        {
+            var impar = 0;
+
+            foreach (int numero in ar)
+            {
+                if (numero % 2 == 0)
+                {
+                    impar++;
+
+                }
+
+            }
+
+            return "A quantidade de numeros pares são: " + impar;
+
+        }
+    }
+
+
+    
+
+}
